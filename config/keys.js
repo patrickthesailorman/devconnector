ab8f225b37a2159aa9a750fe4f7343fdbe1e4ce9
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-25 19:32:40 -0400

    Add keys.js

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://patrick:abcd1234@ds113693.mlab.com:13693/devconnect"
+};
--- server.js
+++ server.js
@@ -6,4 +6,4 @@ app.get("/", (req, res) => res.send("Hello World!"));
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log("Server running on port ${port}"));
+app.listen(port, () => console.log(`Server running on port ${port}`));

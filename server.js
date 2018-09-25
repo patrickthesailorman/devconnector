680b0502ab8269a5c32f75dc446b32c2f70d1b03
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-25 19:41:01 -0400

    Add mongoURI

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+// DB Config
+const db = require("./config/keys").mongoURI;
+
 app.get("/", (req, res) => res.send("Hello World!"));
 
 const port = process.env.PORT || 5000;

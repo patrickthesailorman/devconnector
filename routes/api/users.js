9b95a7c52c30deb4b2e1c990d3f27a6a92006ca8
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-28 21:48:28 -0400

    Add bcrypt

--- routes/api/users.js
+++ routes/api/users.js
@@ -32,8 +32,18 @@ router.post("/register", (req, res) => {
           avatar,
           password: req.body.password
         });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if(err) throw err;
+            newUser.password = hash;
+            newUser.save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));  
+          })
+        })
       }
-    })
+    });
 });
 
 module.exports = router;

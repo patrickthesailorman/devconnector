2c4143fe30ed8c5452abd384a41930abd76d548a
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-28 21:28:48 -0400

    Add body-parser middleware

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const posts = require("./routes/api/posts");
 
 const app = express();
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // DB Config
 const db = require("./config/keys").mongoURI;
 

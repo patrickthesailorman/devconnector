fb0979c70b2dfdb722043d71bae939492cff062d
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-29 22:05:49 -0400

    Add login

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const gravatar = require('gravatar');
-const bcrypt = require('bcryptjs');
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
 // Load User model
 const User = require("../../models/User");
@@ -15,35 +15,43 @@ router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 // @desc    Register User
 // @access  Public
 router.post("/register", (req, res) => {
-  User.findOne({ email: req.body.email })0
-    .then(user => {
-      if(user) {
-        return res.status(400).json({email: 'Email already exists'});
-      } else {
-        const avatar = gravatar.url(req.body.email, { 
-          s: '200', // Size
-          r: 'pg', // Rating
-          d: 'mm' // Default
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", // Size
+        r: "pg", // Rating
+        d: "mm" // Default
       });
 
-        const newUser = new User({
-          name: req.body.name,
-          email: req.body.email,
-          avatar,
-          password: req.body.password
-        });
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
 
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if(err) throw err;
-            newUser.password = hash;
-            newUser.save()
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
             .then(user => res.json(user))
-            .catch(err => console.log(err));  
-          })
-        })
-      }
-    });
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
+
+// @route   GET api/users/login
+// @desc    Login User / Returning JWT Token
+// @access  Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const pasword = req.body.password;
 });
 
 module.exports = router;

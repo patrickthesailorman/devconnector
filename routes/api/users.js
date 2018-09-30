ff3d1853593ae76e9dd5226e136b2082c9edbcda
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-29 23:00:55 -0400

    refactor JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -70,7 +70,17 @@ router.post("/login", (req, res) => {
         const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT Payload
 
         // Sign Token
-        jwt.sign(payload, keys.secretOrKey);
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer" + token
+            });
+          }
+        ); // 1 hr.
       } else {
         return res.status(400).json({ password: "Password incorrect" });
       }

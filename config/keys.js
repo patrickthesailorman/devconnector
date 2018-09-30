7f3e3f6dc643d5ae76c054d4504d311960713747
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-29 22:56:46 -0400

    Add expiresIn

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://patrick:abcd1234@ds113693.mlab.com:13693/devconnect"
+  mongoURI: "mongodb://patrick:abcd1234@ds113693.mlab.com:13693/devconnect",
+  secretOrKey: "secret"
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
 
 // Load User model
 const User = require("../../models/User");
@@ -56,6 +57,7 @@ router.post("/login", (req, res) => {
 
   // Find user by email
   User.findOne({ email }).then(user => {
+    // Check for user
     if (!user) {
       return res.status(404).json({ email: "User not found" });
     }
@@ -63,7 +65,12 @@ router.post("/login", (req, res) => {
     // Check Password
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
-        res.json({ msg: "Success" });
+        // User Matched
+
+        const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT Payload
+
+        // Sign Token
+        jwt.sign(payload, keys.secretOrKey);
       } else {
         return res.status(400).json({ password: "Password incorrect" });
       }

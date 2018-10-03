f7eceb116df0d909fe849d53f73a93581f169c8c
Patrick McCarty <patrickthesailorman@gmail.com>
2018-10-02 20:14:43 -0400

    Add ValRegInput to register route

--- routes/api/users.js
+++ routes/api/users.js
@@ -21,6 +21,8 @@ router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 // @desc    Register User
 // @access  Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
       return res.status(400).json({ email: "Email already exists" });

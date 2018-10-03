38831bfcc86c19405a1f299dde6e37dfcf65590a
Patrick McCarty <patrickthesailorman@gmail.com>
2018-10-02 20:12:15 -0400

    Add validateRegisterInput

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+// Load Input Validation
+const validateRegisterInput = require("../../validation/register");
+
 // Load User model
 const User = require("../../models/User");
 

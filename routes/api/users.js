30681067199afb1d9e325b29f70f25e3c2398ae2
Patrick McCarty <patrickthesailorman@gmail.com>
2018-10-04 15:44:48 -0400

    email exists

--- routes/api/users.js
+++ routes/api/users.js
@@ -31,7 +31,7 @@ router.post("/register", (req, res) => {
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
       errors.email = "Email already exists";
-      return res.status(400).json({ email: "Email already exists" });
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200", // Size

00bed3d6eebe32ec95de02fe93daec81b97576be
Patrick McCarty <patrickthesailorman@gmail.com>
2018-10-05 23:09:09 +0200

    name is required

--- validation/register.js
+++ validation/register.js
@@ -7,7 +7,9 @@ module.exports = function validateRegisterInput(data) {
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
     errors.name = "Name must be between 2 and 30 characters";
   }
-
+  if (validator.isEmpty(data.name)) {
+    errors.name = "Name field is required";
+  }
   return {
     errors,
     isValid: isEmpty(errors)

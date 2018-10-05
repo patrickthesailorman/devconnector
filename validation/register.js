9971a4b400a097f8afd364eb4307a0d1903565ce
Patrick McCarty <patrickthesailorman@gmail.com>
2018-10-05 23:13:52 +0200

    Add data.name

--- validation/register.js
+++ validation/register.js
@@ -4,6 +4,8 @@ const isEmpty = require("./is-empty");
 module.exports = function validateRegisterInput(data) {
   let errors = {};
 
+  data.name = !isEmpty(data.name) ? data.name : "";
+
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
     errors.name = "Name must be between 2 and 30 characters";
   }

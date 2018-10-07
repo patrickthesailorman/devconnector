0517abd237e18f09f4c2387a87c74ec216c10157
Patrick McCarty <patrickthesailorman@gmail.com>
2018-10-08 00:12:18 +0200

    Add password required

--- validation/register.js
+++ validation/register.js
@@ -9,12 +9,23 @@ module.exports = function validateRegisterInput(data) {
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
     errors.name = "Name must be between 2 and 30 characters";
   }
+
   if (validator.isEmpty(data.name)) {
     errors.name = "Name field is required";
   }
+
+  if (validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  }
+
   if (validator.isEmail(data.email)) {
     errors.email = "Email is invalid";
   }
+
+  if (validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+
   return {
     errors,
     isValid: isEmpty(errors)

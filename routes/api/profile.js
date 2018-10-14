038691073cd8e7094bf35c1a02ca23175929ff79
Patrick McCarty <patrickthesailorman@gmail.com>
2018-10-14 13:49:22 -0400

    Bring in Validation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
 
+// Load Validation
+const validateProfileInput = require("../../validation/profile");
+
 // Load Profile Model
 const Profile = require("../../models/Profile");
 // Load User Profile

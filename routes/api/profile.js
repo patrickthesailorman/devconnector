555a2c115fd405f6861cc63356d2173eda0b6c80
Patrick McCarty <patrickthesailorman@gmail.com>
2018-10-16 21:52:54 -0400

    Add .catch

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -52,7 +52,10 @@ router.get("/handle/:handle", (req, res) => {
         errors.profile = "There is no profile for this user";
         res.status(404).json(errors);
       }
-    });
+
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
 });
 
 // @route   POST api/profile

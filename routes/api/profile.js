44e5da5e24d3c24c172771192c40c7347b720beb
Patrick McCarty <patrickthesailorman@gmail.com>
2018-10-17 20:55:01 -0400

    Add all profiles route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -38,6 +38,25 @@ router.get(
   }
 );
 
+// @route   GET api/profile/all
+// @desc    Get all profile
+// @access  Public
+router.get("/all", (req, res) => {
+  const errors = {};
+
+  Profile.find()
+    .populate("user", ["name", "avatar"])
+    .then(profiles => {
+      if (!profiles) {
+        errors.noprofile = "There are no profiles";
+        return res.status(404).json(errors);
+      }
+
+      res.json(profiles);
+    })
+    .catch(err => res.status(404).json({ profile: "There are no profiles" }));
+});
+
 // @route   GET api/profile/handle/:handle
 // @desc    Get profile by handle
 // @access  Public

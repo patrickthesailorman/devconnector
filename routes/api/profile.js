b58e45b5a024268337e65257957d3b356dd40f31
Patrick McCarty <patrickthesailorman@gmail.com>
2018-10-17 20:47:16 -0400

    Change err to message

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -75,7 +75,9 @@ router.get("/user/:user_id", (req, res) => {
 
       res.json(profile);
     })
-    .catch(err => res.status(404).json(err));
+    .catch(err =>
+      res.status(404).json({ profile: "There is no profile for this user" })
+    );
 });
 
 // @route   POST api/profile

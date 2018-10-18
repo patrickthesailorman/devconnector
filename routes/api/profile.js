c5f3b979d77ba69c59828a0ccc4c8d738d533207
Patrick McCarty <patrickthesailorman@gmail.com>
2018-10-18 17:35:01 -0400

    Add validation errors to experience route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -171,6 +171,14 @@ router.post(
   "/experience",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validateExperienceInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
     Profile.findOne({ user: req.user.id }).then(profile => {
       const newExp = {
         title: req.body.title,

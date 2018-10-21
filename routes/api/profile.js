c2afeb5e3fd86c4b1d6b6cc122ef2720b22e282a
Patrick McCarty <patrickthesailorman@gmail.com>
2018-10-21 13:58:04 -0400

    Add delete experience route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -233,4 +233,28 @@ router.post(
   }
 );
 
+// @route   DELETE api/profile/experience
+// @desc    Deleyte experience from profile
+// @access  Private
+router.post(
+  "/experience/:exp_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        // Get remove index
+        const removeIndex = profile.experience
+          .map(item => item.id)
+          .indexOf(req.params.exp_id);
+
+        // Splice out of array
+        profile.experience.splice(removeIndex, 1);
+
+        // Save
+        profile.save().then(profile => res.json(profile));
+      })
+      .caatch(err => res.status(404).json(err));
+  }
+);
+
 module.exports = router;

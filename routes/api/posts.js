1d198b7772c3c658aef35313cbf2fb295c7da753
Patrick McCarty <patrickthesailorman@gmail.com>
2018-10-25 19:08:12 -0400

    Add get posts route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -14,6 +14,15 @@ const validatePostInput = require("../../validation/post");
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
 
+// @route   GET api/posts
+// @desc    Get posts
+// @access  Public
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts));
+});
+
 // @route   POST api/posts
 // @desc    Create post
 // @access  Private

feaf977deaed628acebd2435c997773d3d872250
Patrick McCarty <patrickthesailorman@gmail.com>
2018-10-25 19:09:24 -0400

    Add .catch

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -20,7 +20,8 @@ router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
 router.get("/", (req, res) => {
   Post.find()
     .sort({ date: -1 })
-    .then(posts => res.json(posts));
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404));
 });
 
 // @route   POST api/posts

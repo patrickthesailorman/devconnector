6c1df88723923e5921c838c88dcd2b3446b5ef7d
Patrick McCarty <patrickthesailorman@gmail.com>
2018-10-25 19:14:51 -0400

    Add get post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -24,6 +24,15 @@ router.get("/", (req, res) => {
     .catch(err => res.status(404));
 });
 
+// @route   GET api/posts/:id
+// @desc    Get post by id
+// @access  Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err => res.status(404));
+});
+
 // @route   POST api/posts
 // @desc    Create post
 // @access  Private

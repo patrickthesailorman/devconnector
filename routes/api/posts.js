cb0e80ba51d3d727af540be983ce6cbde71453f3
Patrick McCarty <patrickthesailorman@gmail.com>
2018-10-27 15:56:36 -0400

    Add delete comment route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -153,8 +153,9 @@ router.post(
     });
   }
 );
+
 // @route   POST api/posts/comment/:id
-// @desc    add comment to post
+// @desc    Add comment to post
 // @access  Private
 router.post(
   "/comment/:id",
@@ -178,9 +179,43 @@ router.post(
         };
 
         // Add to comments array
-        post.comments.unshift(newcomment);
+        post.comments.unshift(newComment);
 
         // Save
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+  }
+);
+
+// @route   Delete api/posts/comment/:id/:comment_id
+// @desc    Remove comment from post
+// @access  Private
+router.delete(
+  "/comment/:id/:comment_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        // Check if the comment exists
+        if (
+          post.comments.filter(
+            comment => comment._id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          return res
+            .status(404)
+            .json({ commentnotexists: "Comment does not exist" });
+        }
+
+        // Get remove index
+        const removeIndex = post.comments
+          .map(item => item._id.toString())
+          .indexOf(req.params.comment_id);
+
+        // Splice out of comments array
+        post.comments.splice(removeIndex, 1);
+
         post.save().then(post => res.json(post));
       })
       .catch(err => res.status(404).json({ postnotfound: "No post found" }));

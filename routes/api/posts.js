17ec0d3ef5273e4c73c6908b15c4cb5e969d2483
Patrick McCarty <patrickthesailorman@gmail.com>
2018-10-27 15:28:15 -0400

    Add comment route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -153,5 +153,30 @@ router.post(
     });
   }
 );
+// @route   POST api/posts/comment/:id
+// @desc    add comment to post
+// @access  Private
+router.post(
+  "/comment/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id
+        };
+
+        // Add to comments array
+        post.comments.unshift(newcomment);
+
+        // Save
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+  }
+);
 
 module.exports = router;

c244755b58dc95afb47251ad2a4a03383534da21
Patrick McCarty <patrickthesailorman@gmail.com>
2018-10-29 12:48:29 -0400

    Change a tags to Links

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,15 +1,14 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { linkSync } from "fs";
 
 class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
         <div className="container">
-          <a className="navbar-brand" href="landing.html">
+          <Link className="navbar-brand" to="/landing">
             DevConnector
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -22,10 +21,10 @@ class Navbar extends Component {
           <div className="collapse navbar-collapse" id="mobile-nav">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-                <a className="nav-link" href="profiles.html">
+                <Link className="nav-link" to="/profiles">
                   {" "}
                   Developers
-                </a>
+                </Link>
               </li>
             </ul>
 

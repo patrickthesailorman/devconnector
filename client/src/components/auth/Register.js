4a8265db14d3de6b1eb87a1b11f0416deef1bd17
Patrick McCarty <patrickthesailorman@gmail.com>
2018-10-29 14:53:24 -0400

    Add onChange()

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -10,6 +10,12 @@ class Register extends Component {
       password2: "",
       errors: {}
     };
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   render() {
@@ -30,6 +36,7 @@ class Register extends Component {
                     placeholder="Name"
                     name="name"
                     value={this.state.name}
+                    onChange={this.onChange}
                   />
                 </div>
                 <div className="form-group">
@@ -39,6 +46,7 @@ class Register extends Component {
                     placeholder="Email Address"
                     name="email"
                     value={this.state.email}
+                    onChange={this.onChange}
                   />
                   <small classNameName="form-text text-muted">
                     This site uses Gravatar so if you want a profile image, use
@@ -52,6 +60,7 @@ class Register extends Component {
                     placeholder="Password"
                     name="password"
                     value={this.state.password}
+                    onChange={this.onChange}
                   />
                 </div>
                 <div className="form-group">
@@ -61,6 +70,7 @@ class Register extends Component {
                     placeholder="Confirm Password"
                     name="password2"
                     value={this.state.password2}
+                    onChange={this.onChange}
                   />
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />

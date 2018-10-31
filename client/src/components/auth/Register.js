485f9c27ba9c0cf2da42481b93546e4ea0c962d2
Patrick McCarty <patrickthesailorman@gmail.com>
2018-10-31 17:12:33 -0400

    Add componentWillReceiveProps

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -19,6 +19,12 @@ class Register extends Component {
     this.onSubmit = this.onSubmit.bind(this);
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }

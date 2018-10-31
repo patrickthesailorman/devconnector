9ac555bd2137511e6fde51903103cf7df635cd9e
Patrick McCarty <patrickthesailorman@gmail.com>
2018-10-31 17:21:54 -0400

    remove test code

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -45,11 +45,8 @@ class Register extends Component {
   render() {
     const { errors } = this.state;
 
-    const { user } = this.props.auth;
-
     return (
       <div className="register">
-        {user ? user.name : null}
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">

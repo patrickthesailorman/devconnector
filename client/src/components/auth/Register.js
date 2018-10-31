40c740de40a7bb0b70037fd6eebbdf7fd02493e1
Patrick McCarty <patrickthesailorman@gmail.com>
2018-10-31 17:28:35 -0400

    Add PropTypes.object.isrequired

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -131,7 +131,8 @@ class Register extends Component {
 
 Register.propTypes = {
   registerUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({

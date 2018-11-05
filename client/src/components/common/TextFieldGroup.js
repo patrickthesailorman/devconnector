2f477b7b40882a3fb5b8eb72a4e70734c45317f6
Patrick McCarty <patrickthesailorman@gmail.com>
2018-11-04 21:29:37 -0500

    Add input form

--- client/src/components/common/TextFieldGroup.js
+++ client/src/components/common/TextFieldGroup.js
@@ -1,4 +1,6 @@
 import React from "react";
+import classnames from "classnames";
+import propTypes from "prop-types";
 
 const TextFieldGroup = ({
   mame,
@@ -11,7 +13,21 @@ const TextFieldGroup = ({
   onChange,
   disabled
 }) => {
-  return <div />;
+  return (
+    <div className="form-group">
+      <input
+        type="email"
+        className={classnames("form-control form-control-lg", {
+          "is-invalid": errors.email
+        })}
+        placeholder="Email Address"
+        name="email"
+        value={this.state.email}
+        onChange={this.onChange}
+      />
+      {errors.email && <div className="invalid-feedback">{errors.email} </div>}
+    </div>
+  );
 };
 
 export default TextFieldGroup;

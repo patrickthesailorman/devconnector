33f46e8ea0a90c6e9c627432615ecb38894da491
Patrick McCarty <patrickthesailorman@gmail.com>
2018-11-06 12:03:41 -0500

    Add PROFILE_LOADING

--- client/src/reducers/profileReducer.js
+++ client/src/reducers/profileReducer.js
@@ -8,6 +8,11 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case PROFILE_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
     default:
       return state;
   }

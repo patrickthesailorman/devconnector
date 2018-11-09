a3c09c71653ed42c81f264647ef710bc1c75e785
Patrick McCarty <patrickthesailorman@gmail.com>
2018-11-08 21:20:31 -0500

    Add ProfileActions.js

--- client/src/components/dashboard/ProfileActions.js
+++ client/src/components/dashboard/ProfileActions.js
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom";
 
 const ProfileActions = () => {
   return (
-    <div class="btn-group mb-4" role="group">
-      <a href="edit-profile.html" class="btn btn-light">
-        <i class="fas fa-user-circle text-info mr-1" /> Edit Profile
-      </a>
-      <a href="add-experience.html" class="btn btn-light">
-        <i class="fab fa-black-tie text-info mr-1" />
+    <div className="btn-group mb-4" role="group">
+      <Link to="/edit-profile" className="btn btn-light">
+        <i className="fas fa-user-circle text-info mr-1" /> Edit Profile
+      </Link>
+      <Link to="/add-experience" className="btn btn-light">
+        <i className="fab fa-black-tie text-info mr-1" />
         Add Experience
-      </a>
-      <a href="add-education.html" class="btn btn-light">
-        <i class="fas fa-graduation-cap text-info mr-1" />
+      </Link>
+      <Link to="/add-education" className="btn btn-light">
+        <i className="fas fa-graduation-cap text-info mr-1" />
         Add Education
-      </a>
+      </Link>
     </div>
   );
 };

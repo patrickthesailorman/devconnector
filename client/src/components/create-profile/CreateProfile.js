0194e382daf7fb5d6e2cc9877062adb28541ef64
Patrick McCarty <patrickthesailorman@gmail.com>
2018-11-09 19:17:24 -0500

    Add profileData

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -5,6 +5,7 @@ import TextFieldGroup from "../common/TextFieldGroup";
 import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
 import InputGroup from "../common/InputGroup";
 import SelectListGroup from "../common/SelectListGroup";
+import createProfile from "../../actions/profileActions";
 
 class CreateProfile extends Component {
   constructor(props) {
@@ -40,7 +41,21 @@ class CreateProfile extends Component {
   onSubmit(e) {
     e.preventDefault();
 
-    console.log("submit");
+    const profiledata = {
+      handle: this.state.handle,
+      company: this.state.company,
+      website: this.state.website,
+      location: this.state.location,
+      status: this.state.status,
+      skills: this.state.skills,
+      githubusername: this.state.githubusername,
+      bio: this.state.bio,
+      twitter: this.state.twitter,
+      facebook: this.state.facebook,
+      linkedin: this.state.linkedin,
+      youtube: this.state.youtube,
+      instagram: this.state.instagram
+    };
   }
 
   onChange(e) {
@@ -232,4 +247,7 @@ const mapStateToProps = state => ({
   errors: state.errors
 });
 
-export default connect(mapStateToProps)(CreateProfile);
+export default connect(
+  mapStateToProps,
+  { createProfile }
+)(CreateProfile);

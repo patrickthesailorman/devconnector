54233ea8a678cf48c11e6219e6e8030bc5797758
Patrick McCarty <patrickthesailorman@gmail.com>
2018-11-11 13:03:24 -0500

    Add Experience.js

---
+++ client/src/components/dashboard/Experience.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
+
+class Experience extends Component {
+  render() {
+    const experience = this.props.experience.map(exp => {
+      <tr key={exp._id}>
+        <td>{exp.company}</td>
+        <td>{exp.title}</td>
+        <td>
+          {exp.from} - {exp.to}
+        </td>
+        <td>
+          <button className="btn btn-danger">Delete</button>
+        </td>
+      </tr>;
+    });
+    return <div />;
+  }
+}
+
+export default connect(null)(withRouter(Experience));

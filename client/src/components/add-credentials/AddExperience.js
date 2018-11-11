99477a24bf27a1e38a0f82bbe170cdc67b163b1f
Patrick McCarty <patrickthesailorman@gmail.com>
2018-11-10 19:58:40 -0500

    Add AddExperience

---
+++ client/src/components/add-credentials/AddExperience.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import TextFieldGroup from "../common/TextFieldGroup";
+import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
+import { connect } from "react-redux";
+
+class AddExperience extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default AddExperience;

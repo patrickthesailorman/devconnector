66fd7f0c42dcd41584b32dbed80657cd3db9a90e
Patrick McCarty <patrickthesailorman@gmail.com>
2018-11-11 18:23:23 -0500

    Add Profiles.js

---
+++ client/src/components/profiles/Profiles.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import Spinner from "../common/Spinner";
+import { getProfiles } from "../../actions/profileActions";
+
+class Profiles extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+const mapStateToProps = state => ({
+  profile: state.profile
+});
+
+export default connect(
+  null,
+  { getProfiles }
+)(Profiles);

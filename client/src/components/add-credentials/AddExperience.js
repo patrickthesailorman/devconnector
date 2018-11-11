551de088ad3cbf839d6a76250036642cc8de2506
Patrick McCarty <patrickthesailorman@gmail.com>
2018-11-10 19:59:43 -0500

    Add constructor

--- client/src/components/add-credentials/AddExperience.js
+++ client/src/components/add-credentials/AddExperience.js
@@ -3,11 +3,22 @@ import { Link, withRouter } from "react-router-dom";
 import TextFieldGroup from "../common/TextFieldGroup";
 import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
 import { connect } from "react-redux";
+import PropTypes from 'prop-types';
 
 class AddExperience extends Component {
-  render() {
-    return <div />;
-  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      company: '',
+      title: '',
+      location: '',
+      from: '',
+      to: '',
+      current: false,
+      description: '',
+      errors: {},
+      disabled: false
+    };
 }
 
 export default AddExperience;

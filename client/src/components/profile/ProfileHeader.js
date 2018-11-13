39d2a846ebd330ccd0a8621ed0ec75e538a19a07
Patrick McCarty <patrickthesailorman@gmail.com>
2018-11-12 20:45:21 -0500

    Add profileHeader social...rest

--- client/src/components/profile/ProfileHeader.js
+++ client/src/components/profile/ProfileHeader.js
@@ -48,18 +48,45 @@ class ProfileHeader extends Component {
                   </a>
                 )}
 
-                <a className="text-white p-2" href="#">
-                  <i className="fab fa-twitter fa-2x" />
-                </a>
-                <a className="text-white p-2" href="#">
-                  <i className="fab fa-facebook fa-2x" />
-                </a>
-                <a className="text-white p-2" href="#">
-                  <i className="fab fa-linkedin fa-2x" />
-                </a>
-                <a className="text-white p-2" href="#">
-                  <i className="fab fa-instagram fa-2x" />
-                </a>
+                {isEmpty(profile.social && profile.social.facebook) ? null : (
+                  <a
+                    className="text-white p-2"
+                    href={profile.social.facebook}
+                    target="_blank"
+                  >
+                    <i className="fab fa-facebook fa-2x" />
+                  </a>
+                )}
+
+                {isEmpty(profile.social && profile.social.linkedin) ? null : (
+                  <a
+                    className="text-white p-2"
+                    href={profile.social.linkedin}
+                    target="_blank"
+                  >
+                    <i className="fab fa-linkedin fa-2x" />
+                  </a>
+                )}
+
+                {isEmpty(profile.social && profile.social.youtube) ? null : (
+                  <a
+                    className="text-white p-2"
+                    href={profile.social.youtube}
+                    target="_blank"
+                  >
+                    <i className="fab fa-youtube fa-2x" />
+                  </a>
+                )}
+
+                {isEmpty(profile.social && profile.social.instagram) ? null : (
+                  <a
+                    className="text-white p-2"
+                    href={profile.social.instagram}
+                    target="_blank"
+                  >
+                    <i className="fab fa-instagram fa-2x" />
+                  </a>
+                )}
               </p>
             </div>
           </div>

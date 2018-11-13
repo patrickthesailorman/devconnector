e944566413b80c2baae3a5fcd33a754c8920adff
Patrick McCarty <patrickthesailorman@gmail.com>
2018-11-13 18:26:05 -0500

    Add Moment

--- client/src/components/profile/ProfileCreds.js
+++ client/src/components/profile/ProfileCreds.js
@@ -1,7 +1,19 @@
 import React, { Component } from "react";
+import Moment from 'react-moment';
 
 class ProfileCreds extends Component {
   render() {
+    const { experience, education } = this.props;
+
+    const expItems experience.map(exp => (
+      <li key={exp._id} className="list -group-item">
+        <h4>{exp.company}</h4>
+        <p>
+          <Moment format="YYYY/MM/DD">{exp.from}</Moment> - 
+          {exp.to === nul ? (' Now') : (<Moment format="YYYY/MM/DD">{exp.to}</Moment>)}
+        </p>
+      </li>
+    ));
     return (
       <div>
         <h1>TODO: PROFILE CREDS</h1>

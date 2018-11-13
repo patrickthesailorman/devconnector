116440618e9ecc154ecfb1f6cf6d71fba624dfc3
Patrick McCarty <patrickthesailorman@gmail.com>
2018-11-12 19:18:25 -0500

    Add getProfileByHandle

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,7 @@ import ProfileAbout from "./ProfileAbout";
 import ProfileCreds from "./ProfileCreds";
 import ProfileGithub from "./ProfileGithub";
 import Spinner from "../common/Spinner";
+import { getProfileByHandle } from "../../actions/profileActions";
 
 class Profile extends Component {
   render() {

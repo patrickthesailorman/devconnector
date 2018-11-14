29cd5584640dcb048b9ddf3d34160f56b203440a
Patrick McCarty <patrickthesailorman@gmail.com>
2018-11-13 20:10:32 -0500

    Add github clientID

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 class ProfileGithub extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clientId: "33a9d08b2bc372b6e5a6",
+      clientSecret: "daf4887fbe313516830ba36ef06644eab6ea3b9d",
+      count: 5,
+      sort: "created: asc",
+      repos: []
+    };
+  }
   render() {
     return (
       <div>

1713551bb6a329aa891cf50ebfd5b35dac340950
Patrick McCarty <patrickthesailorman@gmail.com>
2018-11-13 20:02:15 -0500

    Add githubusername to Profile.js

--- client/src/components/profile/ProfileCreds.js
+++ client/src/components/profile/ProfileCreds.js
@@ -10,7 +10,7 @@ class ProfileCreds extends Component {
         <h4>{exp.company}</h4>
         <p>
           <Moment format="YYYY/MM/DD">{exp.from}</Moment> -
-          {exp.to === nul ? (
+          {exp.to === null ? (
             " Now"
           ) : (
             <Moment format="YYYY/MM/DD">{exp.to}</Moment>
@@ -36,12 +36,12 @@ class ProfileCreds extends Component {
       </li>
     ));
 
-    const eduItems = education.map(exp => (
+    const eduItems = education.map(edu => (
       <li key={edu._id} className="list -group-item">
         <h4>{edu.school}</h4>
         <p>
           <Moment format="YYYY/MM/DD">{edu.from}</Moment> -
-          {edu.to === nul ? (
+          {edu.to === null ? (
             " Now"
           ) : (
             <Moment format="YYYY/MM/DD">{edu.to}</Moment>

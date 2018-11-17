7f2eba306fd883c9db9297d6390ed58a9f6b2336
Patrick McCarty <patrickthesailorman@gmail.com>
2018-11-17 17:10:41 -0500

    Add PostItem

---
+++ client/src/components/posts/PostItem.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import classnames from 'classnames';
+import { Link } from 'react-router-dom';
+
+export default class PostItem extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+PostItem.propTypes = {
+  post: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(PostItem);

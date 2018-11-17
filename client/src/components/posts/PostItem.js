ce4d40518ddf5930f6bd5e18fb7561b6508485eb
Patrick McCarty <patrickthesailorman@gmail.com>
2018-11-17 18:15:20 -0500

    Add Post render

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -13,26 +13,31 @@ export default class PostItem extends Component {
               <div className="row">
                 <div className="col-md-2">
                   <a href="profile.html">
-                    <img className="rounded-circle d-none d-md-block" src={post.avatar}
+                    <img className="rounded-circle d-none d-md-block"
+                    src={post.avatar}
                       alt="" />
                   </a>
                   <br />
-                  <p className="text-center">John Doe</p>
+                  <p className="text-center">{post.name}</p>
                 </div>
                 <div className="col-md-10">
-                  <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint possimus corporis sunt necessitatibus! Minus
-                    nesciunt soluta suscipit nobis. Amet accusamus distinctio cupiditate blanditiis dolor? Illo perferendis
-                    eveniet cum cupiditate aliquam?</p>
+                  <p className="lead">{post.text}</p>
                   <button type="button" className="btn btn-light mr-1">
                     <i className="text-info fas fa-thumbs-up"></i>
-                    <span className="badge badge-light">4</span>
+                    <span className="badge badge-light">{post.likes.length}</span>
                   </button>
                   <button type="button" className="btn btn-light mr-1">
                     <i className="text-secondary fas fa-thumbs-down"></i>
                   </button>
-                  <a href="post.html" className="btn btn-info mr-1">
+                  <Link to={`/post/${post._id}`} className="btn btn-info mr-1">
                     Comments
-                  </a>
+                  </Link>
+                  {post.user === auth.user.id ? (
+                    <button onClick={this.onDeleteClick.bind(this, post._id)} type="button" 
+                    className="btn-danger mr-1">
+                    <i className="fas fa-times"></i>
+                    </button>
+                  ) : null}
                 </div>
               </div>
             </div>

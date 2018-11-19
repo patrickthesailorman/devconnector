427ade2fba97793ab6c3835431fc230312edaa07
Patrick McCarty <patrickthesailorman@gmail.com>
2018-11-19 09:48:28 -0500

    Add addLike

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 
-import { ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING } from "./types";
+import {
+  ADD_POST,
+  GET_ERRORS,
+  GET_POSTS,
+  POST_LOADING,
+  DELETE_POST
+} from "./types";
 
 // Add Post
 export const addPost = postData => dispatch => {
@@ -40,23 +46,23 @@ export const getPosts = () => dispatch => {
 };
 
 // Get Post
-export const getPost = id => dispatch => {
-  dispatch(setPostLoading());
-  axios
-    .get(`/api/posts/${id}`)
-    .then(res =>
-      dispatch({
-        type: GET_POST,
-        payload: res.data
-      })
-    )
-    .catch(err =>
-      dispatch({
-        type: GET_POST,
-        payload: null
-      })
-    );
-};
+// export const getPost = id => dispatch => {
+//   dispatch(setPostLoading());
+//   axios
+//     .get(`/api/posts/${id}`)
+//     .then(res =>
+//       dispatch({
+//         type: GET_POST,
+//         payload: res.data
+//       })
+//     )
+//     .catch(err =>
+//       dispatch({
+//         type: GET_POST,
+//         payload: null
+//       })
+//     );
+// };
 
 // Delete Post
 export const deletePost = id => dispatch => {
@@ -68,6 +74,19 @@ export const deletePost = id => dispatch => {
         payload: id
       })
     )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Add Like
+export const addLike = id => dispatch => {
+  axios
+    .post(`/api/posts/like/${id}`)
+    .then(res => dispatch(getPosts()))
     .catch(err =>
       dispatch({
         type: GET_ERRORS,

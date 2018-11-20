52d32e9cee973e91e59773a8d597f89a9142eb65
Patrick McCarty <patrickthesailorman@gmail.com>
2018-11-20 17:42:41 -0500

    Add comment feed div

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -7,7 +7,30 @@ class CommentItem extends Component {
   render() {
     const { comment, postId, auth } = this.props;
 
-    return <div />;
+    return (
+      <div className="card card-body mb-3">
+        <div className="row">
+          <div className="col-md-2">
+            <a href="profile.html">
+              <img
+                className="rounded-circle d-none d-md-block"
+                src={comment.avatar}
+                alt=""
+              />
+            </a>
+            <br />
+            <p className="text-center">Kevin Smith</p>
+          </div>
+          <div className="col-md-10">
+            <p className="lead">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+              possimus corporis sunt necessitatibus! Minus nesciunt soluta
+              suscipit nobis.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 

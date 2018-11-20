761132aec9e40085534ce9d4908286a2b56b27ae
Patrick McCarty <patrickthesailorman@gmail.com>
2018-11-20 17:35:18 -0500

    add CommentItem connect

---
+++ client/src/components/post/CommentItem.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { deleteComment } from "../../actions/postActions";
+
+class CommentItem extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default connect()(CommentItem);

367e0fa39461c05ae79fabaf61f8fef6e5b92989
Patrick McCarty <patrickthesailorman@gmail.com>
2018-11-19 19:05:18 -0500

    Add mapStateToProps

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import Spinner from "../common/Spinner";
+import { getPost } from "../../actions/postActions";
 
 class Post extends Component {
   render() {
@@ -10,4 +14,11 @@ class Post extends Component {
   }
 }
 
-export default Post;
+const mapStateToProps = state => ({
+  post: state.post
+});
+
+export default connect(
+  mapStateToProps,
+  { getPost }
+)(Post);

6bef5b0e100ff85096fc405605ab1e4cbae55934
Patrick McCarty <patrickthesailorman@gmail.com>
2018-11-21 11:25:28 -0500

    Add commentFeed

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -24,7 +24,7 @@ class Post extends Component {
         <div>
           <PostItem post={post} showActions={false} />
           <CommentForm postId={post._id} />
-          {/* <CommentFeed postId={post._id} comments={post.comments} /> */}
+          <CommentFeed postId={post._id} comments={post.comments} />
         </div>
       );
     }
